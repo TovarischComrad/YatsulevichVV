dfd185b25ae2746bb82313c0023daede752f3426
Владимир Яцулевич <vovas53@yandex.ru>
2024-12-27 23:20:24 +0300

    Test links

--- js/header.js
+++ js/header.js
@@ -6,7 +6,7 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <header class="header">
-                <h1><a class="a2" href="index.html">Конспектики.ру</a></h1>
+                <h1><a class="a2" href="/YatsulevichVV/index.html">Конспектики.ру</a></h1>
                 <div class="contact-info">
                     <div>Email: yatsulevichvv@outlook.com</div>
                     <div>+7 937 965 23 63</div>
