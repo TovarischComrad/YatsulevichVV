e4350d38db8af074652cbd5735a265d6d5a2fea4
Владимир Яцулевич <vovas53@yandex.ru>
2024-12-28 18:02:30 +0300

    Mobile table of content

---
+++ js/mobile.js
@@ -0,0 +1,7 @@
+function TableOfContent() {
+    document.getElementsByClassName("tc_mobile")[0].style.display = "block";
+}
+
+function TableOfContentClose() {
+    document.getElementsByClassName("tc_mobile")[0].style.display = "none";
+}
