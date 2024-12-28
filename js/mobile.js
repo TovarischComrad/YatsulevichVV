81b4423ff96b7d4034559d30003945c93c8ee459
Владимир Яцулевич <vovas53@yandex.ru>
2024-12-28 20:59:55 +0300

    Table of content change

--- js/mobile.js
+++ js/mobile.js
@@ -1,7 +1,15 @@
 function TableOfContent() {
     document.getElementsByClassName("tc_mobile")[0].style.display = "block";
+
+    document.getElementsByClassName("header")[0].style.background = "#74A0B6";
+    document.getElementsByClassName("footer")[0].style.background = "#74A0B6";
+    document.getElementsByClassName("main_content")[0].style.background = "#CCCCCC";
 }
 
 function TableOfContentClose() {
     document.getElementsByClassName("tc_mobile")[0].style.display = "none";
+
+    document.getElementsByClassName("header")[0].style.background = "#91C8E4";
+    document.getElementsByClassName("footer")[0].style.background = "#91C8E4";
+    document.getElementsByClassName("main_content")[0].style.background = "#FFFFFF";
 }
