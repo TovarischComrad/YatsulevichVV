ebbb903539bff5fbbe3f7ed691533444f3877e90
Владимир Яцулевич <vovas53@yandex.ru>
2025-01-17 02:47:39 +0300

    Make json database

---
+++ js/dynamic_tc.js
@@ -0,0 +1,56 @@
+class TableOfContent extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+        fetch('/YatsulevichVV/source/json/db.json')
+            .then((response) => response.json())
+            .then((json) => {
+        
+                var path = window.location.pathname;
+                var fileName = path.substring(path.lastIndexOf('/') + 1);
+                var type = json['types'][fileName];
+                var html = Object.keys(json['db'][type]);
+                var name = Object.values(json['db'][type]);
+
+                var s = `
+                    <div class="table_of_content">
+                        <h2>Разделы</h2>
+                        <nav class="nav-links">
+                `;
+                var left = "<a href=\"";
+                var center = "\">";
+                var right = "</a>";
+                
+                for (var i = 0; i < html.length; i++) {
+                    s += left + html[i] + center + name[i] + right;
+                }
+                s += `
+                        </nav>
+                    </div>
+                `
+                this.innerHTML = s;
+            });
+    }
+}
+  
+customElements.define('table-of-content', TableOfContent);
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 650) {
+        document.getElementsByClassName("table_of_content")[0].style.display = "block";
+    }
+    else {
+        document.getElementsByClassName("table_of_content")[0].style.display = "none";
+    }
+})
+
+window.addEventListener('load', () => {
+    if (window.innerWidth <= 650) {
+        document.getElementsByClassName("table_of_content")[0].style.display = "block";
+    }
+    else {
+        document.getElementsByClassName("table_of_content")[0].style.display = "none";
+    }
+})
