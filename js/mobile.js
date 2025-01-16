5e4b93a64f1f149a157c97801cdfe75b91cc711c
Владимир Яцулевич <vovas53@yandex.ru>
2025-01-17 00:28:17 +0300

    fload table of content

--- js/mobile.js
+++ js/mobile.js
@@ -1,6 +1,8 @@
 function TableOfContentOpen() {
-    document.getElementsByClassName("tc_mobile")[0].style.display = "block";
+    // document.getElementsByClassName("tc_mobile")[0].style.display = "block";
     // document.getElementsByClassName("table_of_content")[0].style.display = "block";
+
+    document.querySelector('.tc_mobile').classList.add('active');
 
     document.getElementsByClassName("header")[0].style.background = "#74A0B6";
     document.getElementsByClassName("footer")[0].style.background = "#74A0B6";
@@ -8,8 +10,9 @@ function TableOfContentOpen() {
 }
 
 function TableOfContentClose() {
-    document.getElementsByClassName("tc_mobile")[0].style.display = "none";
+    // document.getElementsByClassName("tc_mobile")[0].style.display = "none";
     // document.getElementsByClassName("table_of_content")[0].style.display = "none";
+    document.querySelector('.tc_mobile').classList.remove('active');
 
     document.getElementsByClassName("header")[0].style.background = "#91C8E4";
     document.getElementsByClassName("footer")[0].style.background = "#91C8E4";
