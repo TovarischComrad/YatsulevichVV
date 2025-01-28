2df71d749b3c884d41098c11906c23618d47df97
Владимир Яцулевич <vovas53@yandex.ru>
2025-01-29 00:41:15 +0300

    Some changes

--- js/header.js
+++ js/header.js
@@ -6,6 +6,7 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <header class="header">
+                <div class="content-button" onclick="TableOfContentOpen()"><img class="burger" src="/YatsulevichVV/source/image/Vector.svg" width="100%" height="100%"></div>
                 <h1 id="website_header"><a class="a2" href="/YatsulevichVV/index.html">ΔATA.V</a></h1>
                 <div class="contact-info">
                     <div class="link"><a target="_blank" href="https://t.me/YatsulevichVV"><img src="/YatsulevichVV/source/image/tg.svg"></a></div>
