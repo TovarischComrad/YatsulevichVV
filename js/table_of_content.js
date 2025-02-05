4383b5aeabcfd21d01a4cef8008589a79823d397
Владимир Яцулевич <vovas53@yandex.ru>
2025-02-06 00:28:03 +0300

    Page load bugfix

--- js/table_of_content.js
+++ js/table_of_content.js
@@ -11,13 +11,24 @@ function nav_list(json) {
     var html = Object.keys(json['db'][type]);
     var name = Object.values(json['db'][type]);
 
+    var sections = [];
     for (var i = 0; i < html.length; i++) {
         var a = document.createElement("a");
         a.setAttribute("href", html[i]);
         a.innerHTML = name[i];
-        console.log(a);
-        document.getElementById("tc_links").append(a);
-        console.log(document.getElementById("tc_links"));
+        sections.push(a);
+    }
+
+    var lst = document.getElementsByClassName("nav-links");
+    if (lst[0].children.length == 0) {
+        for (var i = 0; i < sections.length; i++) {
+            lst[0].append(sections[i]);
+        }
+    }
+    else {
+        for (var i = 0; i < sections.length; i++) {
+            lst[1].append(sections[i]);
+        }
     }
 }
 
@@ -35,17 +46,15 @@ class TableOfContent extends HTMLElement {
         table_of_content.append(h2);
 
         var nav_links = document.createElement("nav");
-        nav_links.setAttribute("id", "tc_links");
         nav_links.setAttribute("class", "nav-links");
         table_of_content.append(nav_links);
-
-        document.getElementById("main_body").append(table_of_content);
 
         fetch('/YatsulevichVV/source/json/db.json')
             .then((response) => response.json())
             .then((json) => nav_list(json));
+
+        this.append(table_of_content);      
     }
 }
   
 customElements.define('table-of-content', TableOfContent);
-
